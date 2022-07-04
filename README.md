ce52816e6af574045f6cb3acbdeb2e804e44a320
rifkialdi <rifkialdiansyah43@gmail.com>
2022-07-04 15:30:25 +0700

    update token

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Menggunakan metode MVVM :
 - LiveData
 - loggingInterceptor
 - github API
+
+<img src="https://github.com/rifkialdi/GithubUser-Dicoding/blob/master/screenshoot_apps/ss1.jpeg" width="200" align="left">
+<img src="https://github.com/rifkialdi/GithubUser-Dicoding/blob/master/screenshoot_apps/ss2.jpeg" width="200" align="left">
+<img src="https://github.com/rifkialdi/GithubUser-Dicoding/blob/master/screenshoot_apps/ss3.jpeg" width="200" align="left">
+<img src="https://github.com/rifkialdi/GithubUser-Dicoding/blob/master/screenshoot_apps/ss4.jpeg" width="200" align="left">
--- app/src/main/java/com/example/githubuser_dicoding/api/ApiService.kt
+++ app/src/main/java/com/example/githubuser_dicoding/api/ApiService.kt
@@ -8,29 +8,29 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getListUser(): Call<List<ResponseItem>>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<ResponseDetail>
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUser(
         @Query("q") q: String
     ): Call<ResponseSearch>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<ResponseItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowing(
         @Path("username") username: String
     ): Call<List<ResponseItem>>
