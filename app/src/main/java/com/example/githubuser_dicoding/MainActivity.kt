6a4f7f7e88161b6e2de703972ce764aada852ca4
rifkialdi <rifkialdiansyah43@gmail.com>
2022-07-04 13:06:12 +0700

    implementation viewmodel,livedata in mainActivity

--- app/src/main/java/com/example/githubuser_dicoding/MainActivity.kt
+++ app/src/main/java/com/example/githubuser_dicoding/MainActivity.kt
@@ -8,12 +8,14 @@ import android.util.Log
 import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubuser_dicoding.adapter.ListAdapter
 import com.example.githubuser_dicoding.api.ApiConfig
 import com.example.githubuser_dicoding.api.ResponseItem
 import com.example.githubuser_dicoding.api.ResponseSearch
 import com.example.githubuser_dicoding.databinding.ActivityMainBinding
+import com.example.githubuser_dicoding.viewmodel.MainViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,13 +24,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var data: ArrayList<ResponseItem>
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        findListUserGithub()
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.findListUser.observe(this) { Responseitem ->
+            showRecycler(Responseitem)
+        }
+        mainViewModel.searchListUser.observe(this) { Responsesearch ->
+            showRecycler(Responsesearch.items)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -43,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
-                findSearchUser(query)
+                mainViewModel.findSearchUser(query)
                 searchView.clearFocus()
                 return true
             }
@@ -55,38 +65,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    fun findListUserGithub() {
-        val client = ApiConfig.getApiService().getListUser()
-        client.enqueue(object : Callback<List<ResponseItem>> {
-            override fun onResponse(call: Call<List<ResponseItem>>, response: retrofit2.Response<List<ResponseItem>>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()!!
-                    showRecycler(responseBody)
-                }
-            }
 
-            override fun onFailure(call: Call<List<ResponseItem>>, t: Throwable) {
-            }
-        })
-    }
-
-    fun findSearchUser(username: String) {
-        val client = ApiConfig.getApiService().getSearchUser(username)
-        client.enqueue(object : Callback<ResponseSearch> {
-            override fun onResponse(
-                call: Call<ResponseSearch>,
-                response: Response<ResponseSearch>
-            ) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()!!
-                    showRecycler(responseBody.items)
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseSearch>, t: Throwable) {
-            }
-        })
-    }
 
     fun showRecycler(value: List<ResponseItem>) {
         data = arrayListOf()
---
+++ app/src/main/java/com/example/githubuser_dicoding/viewmodel/MainViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.githubuser_dicoding.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubuser_dicoding.api.ApiConfig
+import com.example.githubuser_dicoding.api.ResponseItem
+import com.example.githubuser_dicoding.api.ResponseSearch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+    private val _findListUser = MutableLiveData<List<ResponseItem>>()
+    val findListUser: LiveData<List<ResponseItem>> = _findListUser
+
+    private val _searchListUser = MutableLiveData<ResponseSearch>()
+    val searchListUser: LiveData<ResponseSearch> = _searchListUser
+
+    init {
+        findListUserGithub()
+    }
+
+    fun findListUserGithub() {
+        val client = ApiConfig.getApiService().getListUser()
+        client.enqueue(object : Callback<List<ResponseItem>> {
+            override fun onResponse(call: Call<List<ResponseItem>>, response: retrofit2.Response<List<ResponseItem>>) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()!!
+                    _findListUser.value = responseBody
+                }
+            }
+
+            override fun onFailure(call: Call<List<ResponseItem>>, t: Throwable) {
+            }
+        })
+    }
+
+    fun findSearchUser(username: String) {
+        val client = ApiConfig.getApiService().getSearchUser(username)
+        client.enqueue(object : Callback<ResponseSearch> {
+            override fun onResponse(
+                call: Call<ResponseSearch>,
+                response: Response<ResponseSearch>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()!!
+                    _searchListUser.value = responseBody
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSearch>, t: Throwable) {
+            }
+        })
+    }
+}
